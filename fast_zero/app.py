6391b8574ea6c3e56639fb9d82c967b2330c40a2
FabricioPython <fabricio.mh@outlook.com>
2024-07-01 12:27:33 -0300

    testes 404 e get user

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -10,11 +10,31 @@ app = FastAPI()
 database = []
 
 
+# raiz
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
 
 
+# ver um usuario - GET
+@app.get('/users/{id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def read_user(id: int):
+    if id not in database:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user = database[id - 1]
+    return user
+
+
+# ver todos usuario - GET
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+# cadastrar usuario - POST
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
@@ -24,12 +44,10 @@ def create_user(user: UserSchema):
     return user_with_id
 
 
-@app.get('/users/', response_model=UserList)
-def read_users():
-    return {'users': database}
-
-
-@app.put('/users/{user_id}', response_model=UserPublic)
+# atualizar usuario - PUT
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
 def update_user(user_id: int, user: UserSchema):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
@@ -42,7 +60,10 @@ def update_user(user_id: int, user: UserSchema):
     return user_with_id
 
 
-@app.delete('/users/{user_id}', response_model=Message)
+# deletar usuario - DELETE
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message
+)
 def delete_user(user_id: int):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,13 @@
 from http import HTTPStatus
 
-
+# teste - rota inicial
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
 
-
+# teste - criar usuario
 def test_create_user(client):
     response = client.post(
         '/users/',
@@ -58,9 +58,33 @@ def test_update_user(client):
     }
 
 
-# testde de delete
+# teste de falha de atualização
+# O cliente digitou um id que nao existe.
+def test_update_user_fail(client):
+    response = client.put(
+        '/users/40',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+# teste - delete
 def test_delete_user(client):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+
+# teste - delete - Tentou apagar um id que não existe..
+def test_delete_user_fail(client):
+    response = client.delete('/users/50')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
